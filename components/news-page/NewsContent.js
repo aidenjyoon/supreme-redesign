a5cd8bae7e0d3beb0d2fe60e5dddcffc7abb8f0e
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-05-18 15:32:06 -0700

    rough outline of news page

---
+++ components/news-page/NewsContent.js
@@ -0,0 +1,104 @@
+import Image from "next/image";
+import Logo from "../logo/Logo";
+import styles from "./NewsContent.module.scss";
+
+const NewsContent = () => {
+  return (
+    <>
+      <div className={styles.container__content}>
+        <div className={styles.container__logo}>
+          <Logo />
+          <h2>News</h2>
+        </div>
+        <div className={styles.container__image}>
+          <Image
+            src="/images/bomber-jacket.jpg"
+            width={514}
+            height={585}
+            alt="news image"
+            layout={"responsive"}
+          />
+        </div>
+        <article className={styles.container__news}>
+          <p>date</p>
+          <h1>Title</h1>
+          <p>news content</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>
+            news
+            conasddddddddddddddddddasdfasdfasdfasdddddddddddddddddddddddddddtent
+          </p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+          <p>news conasdddddddddddddddddddddtent</p>
+        </article>
+      </div>
+    </>
+  );
+};
+
+export default NewsContent;

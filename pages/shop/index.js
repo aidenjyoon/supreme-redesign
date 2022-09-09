133d82022cb4122fbc7dd48d4010835ead20913f
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-09-08 22:50:52 -0700

    shop pull  categories from api

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,6 +1,11 @@
 import ShopContent from "../../components/shop/ShopContent";
+import { getAllCategories } from "../../helpers/utils";
 
-const ShopPage = () => {
+const ShopPage = (props) => {
+  const { categories } = props;
+
+  console.log(categories);
+
   return (
     <>
       <ShopContent />
@@ -8,4 +13,16 @@ const ShopPage = () => {
   );
 };
 
+const getStaticProps = async () => {
+  const allCategories = await getAllCategories();
+
+  return {
+    props: {
+      categories: allCategories,
+    },
+    revalidate: 1800,
+  };
+};
+
+export { getStaticProps };
 export default ShopPage;
